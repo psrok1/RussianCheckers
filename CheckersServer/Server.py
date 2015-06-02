c3615982291080e740b0e5dbed007390643feb40
tnazarew <t.nazarewicz94@gmail.com>
2015-06-02 20:52:04 +0200

    serwer odpowiadajacy na zapytania w Server.py i TopScorers w trakcie budowy

--- CheckersServer/Server.py
+++ CheckersServer/Server.py
@@ -1,9 +1,10 @@
 import tornado.ioloop
 import tornado.web 
 import tornado.websocket
-import Game
-import Checker
-import TopScorers
+import json
+#import Game
+#import Checker
+#import TopScorers
 
 """Klasa dziedziczaca po WebSocketHandler, odpowiada ze komunikacje serwera z przegladarka"""    
 class WSHandler(tornado.websocket.WebSocketHandler):
@@ -14,8 +15,11 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     
     ''' odbiera wiadomosc tekstowa wyslana do serwera'''  
     def on_message(self, message):
-        print 'message' 
-        self.write_message(message)
+	json_data = message;
+	useful_data = json.loads(json_data)
+	self.interpret(useful_data)
+	  
+        #self.write_message(message)
     
     ''' wykonuje sie po zamknieciu polaczenia z serwerem'''
     def on_close(self):
@@ -29,13 +33,37 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     #def get(self):
     #   pass
         #self.render("zrodlostrony.html") #.js tez powinno pociagnac
+    def interpret(self, dict):
+	if dict['message'] == 'rank':
+	    # wez dane z pliku i wyslij :v
+	    message = '{"message": "rank", "times": [100, 200, 300, 400, 500]}'
+	    self.write_message(message)	
+	else: 
+		if dict['message'] == 'hello':	
+		    self.hello(dict["pieces"])
+		else: 
+		    if dict['message'] == 'move':
+	    		self.move(dict['moves'])
+    
+    def hello(self,pieces):
+	if pieces == 'auto' or pieces == 'white':
+	    message = '{"message": "hello", "pieces": "white"}'
+	    self.write_message(message)
+	if pieces == 'black':
+	    message = '{"message": "hello", "pieces": "black"}'
+	    self.write_message(message)
 
+    def move(self,moves):
+	message = {"message": "move", "moves": ["A2", "B3", "C4"]}
+	self.write_message(message)
+	message1 = {"message": "end", "time": 68, "clientWin": "true"}
+	self.write_message(message1)	
+	
 if __name__ == "__main__":
-    game = Game.Game()
-    checker = Checker.Checker('b', True, "1a")
-    topscore = TopScorers.TopScorers()
+    #game = Game.Game()
+    #checker = Checker.Checker('b', True, "1a")
+    #topscore = TopScorers.TopScorers()
     application = tornado.web.Application([
     (r'/', WSHandler)])
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
-
--- CheckersServer/TopScorers.py
+++ CheckersServer/TopScorers.py
@@ -10,24 +10,49 @@ class TopScorers(object):
     def __init__(self):
         print 'Konstruktor TopScores'
 	self.scorers = []
+	    
         
     """Wczytuje ostatnio zapisana wersje listy"""
     def read(self):
-        pass
+	target = open('TopScorers')
+	self.scorers = []
+	self.scorers.append([int(x) for x in target.readline().split()])
+	target.close()        
+
+
     
     """Zapisuje obecny stan listy, nadpisujac plik"""
     def update(self):
-        pass
+	target = open('TopScorers', 'w')
+	for s in self.scorers:
+	    target.write(str(s))
+	    target.write(" ")
+        target.close()
     
     """Tworzy pakiet gotowy do wyslania na serwer"""
     def sendingList(self):
-        pass
+	#message = '{"message": "rank", "times": ['+ self.scorers[0] + ', ' + self.scorers[1] + ', ' + self.scorers[2] + ', ' + self.scorers[3] + ', ' + self.scorers[4] + ']}'
+	message = '{"message": "rank", "times": ' + str(self.scorers) + '}'
+        return message
     
     """Sprawdza czy podany czas jest lepszy niz najgorszy wynik na liscie"""
-    def isGoodEnough(self):
-        pass
+    def isGoodEnough(self, time):
+	return time < self.scorers[4] 
     
     """Dodaje wynik oraz dane gracza na odpowiednie miejsce listy"""
-    def addNewTopScorer(self):
-        pass
-    
+    def addNewTopScorer(self, time):
+	for i in range(0,5):
+	    if self.scorers[i] > time:
+	        j = i+1
+	        while j < 5:
+		    self.scorers[j] = self.scorers[j-1]
+	            j = j + 1
+	        self.scorers[i] = time
+
+
+if __name__ == "__main__":
+    top = TopScorers()
+    top.read()
+    print top.sendingList()
+    top.update()	
+
